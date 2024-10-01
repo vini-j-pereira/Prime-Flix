a491bc046aefe371271632443645c0338ef22c13
vini-j-pereira <viniciusjosepereira@gmail.com>
2024-10-01 14:50:26 -0300

    feat: filmes na home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import api from '../../services/api';
+import { Link } from 'react-router-dom'
+import './home.css';
 
 // https://api.themoviedb.org/3/movie/now_playing?api_key=32606ff82573d774d6649212e8016b52&language=pt-BR
 
@@ -17,14 +19,26 @@ function Home() {
                 }
             });
             
+            setFilmes(response.data.results.slice(0, 10));   
+
         }
 
         loadFilmes();
 
     }, [])
     return(
-        <div>
-            <h1>Home</h1>
+        <div className="container">
+            <div className="lista-filmes">
+                {filmes.map((filme)=> {
+                    return(
+                        <article>
+                            <strong>{filme.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}></img>
+                            <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                        </article>
+                    )
+                })}
+            </div>  
         </div>
     )
 }
